e7dbf77bc25659947aafc5ea672093c4997ee61d
zajimenezrosario001 <Za.JimenezRosario001@mymdc.net>
2025-04-23 09:27:20 -0400

    Add files via upload

---
+++ Loading.py
@@ -0,0 +1,24 @@
+def load_choice(choice):
+    """
+    Load a choice from a file.
+    """
+    if choice == "1":
+        choice_1()
+    elif choice == "2":
+        choice_2()
+    elif choice == "3":
+        choice_3()
+    elif choice == "4":
+        choice_4()
+    elif choice == "5": 
+        choice_5()
+    elif choice == "6":
+        choice_6()
+    elif choice == "7":
+        choice_7()
+    elif choice == "8":
+        choice_8()
+    elif choice == "9":
+        choice_9()
+    elif choice == "10":
+        choice_10()
